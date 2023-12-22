577e959cbaf6e744c9732ea8dc824f42bfaeed2f
jddev <hsd.salaheddine@gmail.com>
2023-12-22 12:55:11 +0000

    move some properties from contact obj to applications

--- getEbsData.js
+++ getEbsData.js
@@ -30,11 +30,11 @@ const getEbsData = async () => {
             .query("SELECT * FROM Applications");
 
         // Create a mapping of contacts by email
-        const contactsByEmail = await contacts.recordset.reduce(
+        let contactsByEmail = await contacts.recordset.reduce(
             (map, contact) => {
                 map[contact.PERSONAL_EMAIL] = {
-                    student_reference: contact.STUDENT_REFERENCE,
                     firstname: contact.FIRST_NAME,
+                    student_reference: contact.STUDENT_REFERENCE,
                     email: contact.PERSONAL_EMAIL,
                     lastname: contact.LAST_NAME,
                     date_of_birth: contact.DATE_OF_BIRTH,
@@ -44,7 +44,6 @@ const getEbsData = async () => {
                     zip: contact.POSTAL_CODE || undefined,
                     school_title: contact.SCHOOL_DESCRIPTION || undefined,
                     school_code: contact.SCHOOL_CODE,
-                    // nationality: contact.NATIONALITY,
                     marketing_contact_methods:
                         contact.MARKETING_CONTACT_METHODS || undefined,
                     applications: [],
@@ -55,87 +54,42 @@ const getEbsData = async () => {
         );
 
         // Associate applications with contacts by email
-        // await applications.recordset.forEach((application) => {
-        //     const email = application.PERSONAL_EMAIL;
-        //     if (contactsByEmail[email]) {
-        //         contactsByEmail[email].applications.push({
-        //             unit_id: application.UNIT_ID,
-        //             course_occurrence: application.COURSE_OCCURRENCE,
-        //             course_code: application.COURSE_CODE,
-        //             website_advertised_title: application.WEBTITLE,
-        //             college_code: application.COLLEGE_CODE,
-        //             college_name: application.COLLEGE_FULLNAME,
-        //             org_level_1_code: application.ORG_L1_CODE,
-        //             org_level_1_name: application.ORG_L1_FULLNAME,
-        //             org_level_2_code: application.ORG_L2_CODE,
-        //             org_level_2_name: application.ORG_L2_FULLNAME,
-        //             org_level_3_code: application.ORG_L3_CODE,
-        //             org_level_3_name: application.ORG_L3_FULLNAME,
-        //             application_created_date: application.CREATED_DATE,
-        //             study_location_code:
-        //                 application.STUDY_LOCATION_CODE || undefined,
-        //             study_location_description:
-        //                 application.STUDY_LOCATION_DESCRIPTION || undefined,
-        //             study_location_postcode:
-        //                 application.STUDY_LOCATION_POSTCODE || undefined,
-        //             qualification_type:
-        //                 application.QUALIFICATION_TYPE || undefined,
-        //             stage: application.STAGE,
-        //         });
-        //     }
-        // });
-
-        // Associate applications with contacts by email
-        await applications.recordset.forEach((application) => {
+        await applications_db.recordset.forEach((application) => {
             const email = application.PERSONAL_EMAIL;
+
             if (contactsByEmail[email]) {
                 const contact = contactsByEmail[email];
-
-                // Add contact information to the application object
-                const applicationObject = {
-                    student_reference: contact.student_reference,
-                    firstname: contact.firstname,
-                    lastname: contact.lastname,
-                    email: contact.email,
-                    // ... (Other application fields)
-
-                    // Add application to the applications array
-                    // (if it's not already added, you may want to check for duplicates)
-                    applications: [
-                        ...contact.applications,
-                        {
-                            unit_id: application.UNIT_ID,
-                            course_occurrence: application.COURSE_OCCURRENCE,
-                            unit_id: application.UNIT_ID,
-                            course_occurrence: application.COURSE_OCCURRENCE,
-                            course_code: application.COURSE_CODE,
-                            website_advertised_title: application.WEBTITLE,
-                            college_code: application.COLLEGE_CODE,
-                            college_name: application.COLLEGE_FULLNAME,
-                            org_level_1_code: application.ORG_L1_CODE,
-                            org_level_1_name: application.ORG_L1_FULLNAME,
-                            org_level_2_code: application.ORG_L2_CODE,
-                            org_level_2_name: application.ORG_L2_FULLNAME,
-                            org_level_3_code: application.ORG_L3_CODE,
-                            org_level_3_name: application.ORG_L3_FULLNAME,
-                            application_created_date: application.CREATED_DATE,
-                            study_location_code:
-                                application.STUDY_LOCATION_CODE || undefined,
-                            study_location_description:
-                                application.STUDY_LOCATION_DESCRIPTION ||
-                                undefined,
-                            study_location_postcode:
-                                application.STUDY_LOCATION_POSTCODE ||
-                                undefined,
-                            qualification_type:
-                                application.QUALIFICATION_TYPE || undefined,
-                            stage: application.STAGE,
-                        },
-                    ],
-                };
-
                 // Update the contact's applications array
-                contactsByEmail[email].applications.push(applicationObject);
+                contactsByEmail[email].applications.push({
+                    student_reference: contact.student_reference,
+                    student_first_name: contact.firstname,
+                    student_last_name: contact.lastname,
+                    email: contact.email,
+                    unit_id: application.UNIT_ID,
+                    course_occurrence: application.COURSE_OCCURRENCE,
+                    unit_id: application.UNIT_ID,
+                    course_occurrence: application.COURSE_OCCURRENCE,
+                    course_code: application.COURSE_CODE,
+                    website_advertised_title: application.WEBTITLE,
+                    college_code: application.COLLEGE_CODE,
+                    college_name: application.COLLEGE_FULLNAME,
+                    org_level_1_code: application.ORG_L1_CODE,
+                    org_level_1_name: application.ORG_L1_FULLNAME,
+                    org_level_2_code: application.ORG_L2_CODE,
+                    org_level_2_name: application.ORG_L2_FULLNAME,
+                    org_level_3_code: application.ORG_L3_CODE,
+                    org_level_3_name: application.ORG_L3_FULLNAME,
+                    application_created_date: application.CREATED_DATE,
+                    study_location_code:
+                        application.STUDY_LOCATION_CODE || undefined,
+                    study_location_description:
+                        application.STUDY_LOCATION_DESCRIPTION || undefined,
+                    study_location_postcode:
+                        application.STUDY_LOCATION_POSTCODE || undefined,
+                    qualification_type:
+                        application.QUALIFICATION_TYPE || undefined,
+                    stage: application.STAGE,
+                });
             }
         });
 
